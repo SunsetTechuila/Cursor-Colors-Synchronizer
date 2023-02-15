df7eb3d6b3440c3f7689ba1384498cc757666dac
Sunset <grigory.orlov.set@gmail.com>
2023-02-15 11:28:07 +0500

    repack archive

--- Install.ps1
+++ Install.ps1
@@ -3,16 +3,14 @@
 
 $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$ccsFolder = "$env:LocalAppData\CCS"
 
-if (Test-Path -Path $ccsFolder) {
-	Remove-Item -Path $ccsFolder -Recurse -Force | Out-Null
+if (Test-Path -Path $env:LocalAppData\CCS) {
+	Remove-Item -Path $env:LocalAppData\CCS -Recurse -Force | Out-Null
 }
-New-Item -ItemType Directory -Path $ccsFolder | Out-Null
 
 $uri = 'https://github.com/SunsetTechuila/Cursor-Colors-Synchronizer/releases/latest/download/Cursor-Colors-Synchronizer.zip'
-Invoke-WebRequest -Uri $uri -OutFile $ccsFolder\Cursor-Colors-Synchronizer.zip
-Expand-Archive -Path $ccsFolder\Cursor-Colors-Synchronizer.zip -DestinationPath $ccsFolder
-Remove-Item -Path $ccsFolder\Cursor-Colors-Synchronizer.zip -Force
+Invoke-WebRequest -Uri $uri -OutFile $env:LocalAppData\Cursor-Colors-Synchronizer.zip
+Expand-Archive -Path $env:LocalAppData\Cursor-Colors-Synchronizer.zip -DestinationPath $env:LocalAppData
+Remove-Item -Path $env:LocalAppData\Cursor-Colors-Synchronizer.zip -Force
 
-Start-Process -FilePath powershell -ArgumentList "-ExecutionPolicy Bypass -NoExit -File $ccsFolder\CCS.ps1"
+Start-Process -FilePath powershell -ArgumentList "-ExecutionPolicy Bypass -NoExit -File $env:LocalAppData\CCS\CCS.ps1"
